dc8e5425e45a2b5d35ffd168a1279b1de6036c92
Hao Chen <501018786@qq.com>
2023-02-09 19:37:52 +0800

    Add python version of 081 and 347

---
+++ Binary_Search/347.Top-K-Frequent-Elements/347.Top-K-Frequent-Elements.py
@@ -0,0 +1,27 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        notebook = {}
+        for num in nums:
+            notebook[num] = notebook.setdefault(num, 0) + 1
+        # 找到第 K 个的元素的频率到底是多少
+        left = 0
+        right = len(nums)
+        while left < right:
+            middle = math.ceil((left + right) / 2)
+            if self.freqGreaterOrEqualCount(middle, notebook) >= k:
+                left = middle
+            else:
+                right = middle - 1
+
+        result = []
+        for num in notebook:
+            if notebook[num] >= right:
+                result.append(num)
+        return result
+                
+    def freqGreaterOrEqualCount(self, times, notebook):
+        count = 0
+        for num in notebook:
+            if notebook[num] >= times:
+                count += 1
+        return count
